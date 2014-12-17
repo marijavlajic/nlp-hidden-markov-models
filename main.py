a05ebe2eb4bd48581765c8a1b41cee88d6a9d63a
Marija Vlajic <marija.vlajic@de-mac-015.zanox.com>
2014-12-17 15:00:21 +0100

    Modified main.py to only run CountsGenerator.run() once instead of twice.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from hidden_markov_model import CountsGenerator
 from viterbi_algorithm import RareNGramProcessor, ClassNGramProcessor
 
 def run(train_filename, counts_filename, test_filename):
-    RareNGramProcessor(CountsGenerator.run(train_filename, counts_filename)[0], test_filename)
-    ClassNGramProcessor(CountsGenerator.run(train_filename, counts_filename)[1], test_filename)
+    input_filename = CountsGenerator.run(train_filename, counts_filename)
+    RareNGramProcessor(input_filename[0], test_filename)
+    ClassNGramProcessor(input_filename[1], test_filename)
 
 run(*sys.argv[1:4])
